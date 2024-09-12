32647136781b04e0195484036d331eaf29729012
valerian <50135742+valeian3@users.noreply.github.com>
2024-09-12 12:55:14 +0200

    improve checkbox

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -61,15 +61,31 @@ const TodoItem: FC<TodoItemProps> = ({ todo }) => {
     <div className="flex justify-between mb-4 rounded-2xl py-2 px-4 bg-slate-50 dark:bg-slate-600 transition-colors duration-100">
       <div className="flex flex-col justify-center">
         <div className="flex items-center">
-          <input
-            id={todo.id.toString()}
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => toggleTodoCompletion(todo.id)}
-            className={`w-4 h-4 cursor-pointer text-green-400 bg-slate-200 border-slate-300 rounded-full focus:ring-transparent dark:focus:ring-transparent dark:ring-offset-slate-800 focus:ring-2 dark:bg-slate-700 dark:border-slate-600 
-    appearance-none checked:bg-green-600 checked:border-transparent 
-       `}
-          />
+          <label className="flex items-center cursor-pointer relative">
+            <input
+              id={todo.id.toString()}
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodoCompletion(todo.id)}
+              className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded-full shadow hover:shadow-md border border-slate-300 checked:bg-green-600 checked:border-green-600"
+            />
+            <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3.5 w-3.5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                stroke="currentColor"
+                strokeWidth="1"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </span>
+          </label>
 
           {edit === "content" ? (
             <input
